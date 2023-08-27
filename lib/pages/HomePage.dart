e14f1a4e992ab6dec35f920fd2a2a2308abd703c
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-08-27 14:22:40 +0600

    search bar

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -12,7 +12,33 @@ class MyHomePage extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          // Custom APPbar
           AppBarWidget(),
+
+          // SearchBar
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15
+            ) ,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3)
+                  )
+                ],
+              ),
+            ),
+          
+          )
         ],
       ),
     );
