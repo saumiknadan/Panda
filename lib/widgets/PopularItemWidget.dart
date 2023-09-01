17a5619d68d3a394408e6904277d9cf4ecb93a15
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-09-01 12:33:35 +0600

    Popular Item Single Item full

--- lib/widgets/PopularItemWidget.dart
+++ lib/widgets/PopularItemWidget.dart
@@ -11,31 +11,32 @@ class PopularItemWidget extends StatelessWidget {
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
         child: Row(
           children: [
-            Container(
-              width: 170,
-              height: 225,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  )
-                ]),
+            // for (int i = 0; i < 10; i++)
+              // Single Item
+              Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
                 // Item Insert
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                     children: [
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
                       Container(
                         alignment: Alignment.center,
                         child: Image.asset("lib/images/burger.png"),
                         height: 130,
-                
                       ),
                       Text(
                         "Hot Burger",
@@ -43,11 +44,44 @@ class PopularItemWidget extends StatelessWidget {
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
-                         ),
-                     ],
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Taste our hot burger",
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
-            )
+              ),
+
+           
           ],
         ),
       ),
