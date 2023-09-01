2fb56d160b21457db298ca049a149aedea64c4f9
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-09-01 12:34:31 +0600

    Popular Item with all item

--- lib/widgets/PopularItemWidget.dart
+++ lib/widgets/PopularItemWidget.dart
@@ -81,6 +81,282 @@ class PopularItemWidget extends StatelessWidget {
                 ),
               ),
 
+                 //  Biriyani
+              Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+                // Item Insert
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset("lib/images/biryani.png"),
+                        height: 130,
+                      ),
+                      Text(
+                        "Mutton Khacchi",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Taste Kacchi Biriyani",
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+                //  Chicken Salan
+              Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+                // Item Insert
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset("lib/images/salan.png"),
+                        height: 130,
+                      ),
+                      Text(
+                        "Chicken Salan",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Taste Chicken Salan",
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+        
+        // Pizza
+              Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+                // Item Insert
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset("lib/images/Pizza.png"),
+                        height: 130,
+                      ),
+                      Text(
+                        "BBQ Pizza",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Taste our hot Pizza",
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+        
+        // Drink
+              Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+                // Item Insert
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset("lib/images/drink.png"),
+                        height: 130,
+                      ),
+                      Text(
+                        "Virgin Mojito",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Taste our Virgin Mojito",
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+        
+        
            
           ],
         ),
